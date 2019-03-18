9c5bf6fbe24abf39ac7141f42a38111d25c6baf4
Hardy <Gary.Hardy@ext.saint-gobain.com>
2019-03-18 14:35:15 +0100

    another one for the day

--- testgh.tf
+++ testgh.tf
@@ -1,19 +1,19 @@
-resource "azurerm_resource_group" "testghSG" {
+resource "azurerm_resource_group" "testrgghSG" {
   name     = "acceptanceTestResourceGroup1GH"
   location = "West US"
 }
 
-resource "azurerm_virtual_network" "testsggh" {
+resource "azurerm_virtual_network" "testvnghSG" {
   name                = "acceptanceTestVirtualNetwork1GH"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.testghsg.location}"
-  resource_group_name = "${azurerm_resource_group.testsggh.name}"
+  location            = "${azurerm_resource_group.testrgghSG.location}"
+  resource_group_name = "${azurerm_resource_group.testrgghSG.name}"
 }
 
 resource "azurerm_subnet" "test" {
   name                 = "testsubnet"
-  resource_group_name  = "${azurerm_resource_group.testsggh.name}"
-  virtual_network_name = "${azurerm_virtual_network.testsggh.name}"
+  resource_group_name  = "${azurerm_resource_group.testrgghSG.name}"
+  virtual_network_name = "${azurerm_virtual_network.testvnghSG.name}"
   address_prefix       = "10.0.1.0/24"
   delegation {
     name = "acctestdelegation"
