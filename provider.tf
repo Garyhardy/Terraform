3eeb8fe57db558c6fb6ba49666bda577e5ee3d2e
Hardy <Gary.Hardy@ext.saint-gobain.com>
2019-03-18 11:59:47 +0100

    OK for gh

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+    subscription_id = "a1c92b66-e7f7-4496-bf91-cb434dcf56f7"
+    client_id       = "azure-cli-2019-03-18-10-53-26"
+    client_secret   = "1889113f-72b8-433d-a0a0-cdcce7025c6e"
+    tenant_id       = "e339bd4b-2e3b-4035-a452-2112d502f2ff"
+}
